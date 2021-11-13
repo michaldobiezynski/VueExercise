25a25d917cfd5d5c38575d5fb7f4c75f3f5436b6
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-11-13 17:35:37 +0000

    add fontawesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { createApp } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+library.add(faSearch);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router)
+  .mount('#app');
