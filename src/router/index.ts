ffd72c83235f6758ecba7b99d1239d7afcb88c30
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-11-13 17:15:43 +0000

    make home list the root route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
+import HeroList from '@/views/HeroList.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'HeroList',
+    component: HeroList,
   },
   {
     path: '/about',
