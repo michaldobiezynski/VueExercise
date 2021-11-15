59100dbcaa981d484edb52318651e714880dd716
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-11-15 20:17:23 +0000

    create initial hero details page

---
+++ src/views/HeroDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>HeroDetails</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRoute } from 'vue-router';
+import HerosApi from '@/models/HeroApi';
+
+export default defineComponent({
+  name: 'HeroDetails',
+  props: {},
+  async mounted() {
+    const route = useRoute();
+    console.log(route.query);
+    console.log(await HerosApi.getHeroById('69'));
+  },
+});
+</script>
+
+<style scoped lang="scss">
+</style>
